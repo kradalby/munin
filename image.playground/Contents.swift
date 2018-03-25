604f1b60f478050a59c5aa650b9c47720bd650e2
Kristoffer Dalby <kradalby@kradalby.no>
2018-03-25 20:16:38 +0200

    adds image playground to explore image files

--- image.playground/Contents.swift
+++ image.playground/Contents.swift
@@ -3,3 +3,12 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+let fileURL = NSURL.fileURL(withPath: "/Users/kradalby/Library/Autosave Information/sample/nolocation/20180307-201421-IMG_6006_original.jpg")
+if let imageSource = CGImageSourceCreateWithURL(fileURL as CFURL, nil) {
+    
+    let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil)
+    if let dict = imageProperties as? [String: Any] {
+        print(dict)
+    }
+}
