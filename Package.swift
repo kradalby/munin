355157b65b7fc0867d06228bef57aaef036191d4
Kristoffer Dalby <kradalby@kradalby.no>
2020-06-28 15:07:23 +0100

    Add SwiftGD

--- Package.swift
+++ Package.swift
@@ -9,17 +9,20 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
             name: "munin",
-            targets: ["Munin"]),
+            targets: ["Munin"]
+        ),
         .library(
             name: "MuninKit",
-            targets: ["MuninKit"])
-        ],
+            targets: ["MuninKit"]
+        ),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/kylef/Commander.git", from: "0.9.1"),
         .package(url: "https://github.com/kradalby/Logger.swift.git", from: "0.0.6"),
-        .package(url: "https://github.com/kradalby/Config.swift.git", from: "0.0.1")
+        .package(url: "https://github.com/kradalby/Config.swift.git", from: "0.0.1"),
+        .package(url: "https://github.com/twostraws/SwiftGD.git", from: "2.5.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,14 +33,16 @@ let package = Package(
                 "MuninKit",
                 "Logger",
                 "Config",
-                "Commander"
-                ]),
+                "Commander",
+            ]
+        ),
         .target(
             name: "MuninKit",
             dependencies: [
                 "Logger",
-                "Config"
-                ])
+                "Config",
+                "SwiftGD",
+            ]
+        ),
     ]
-
 )
