68557bb4a667303145685ee4826c449862ee709f
Kristoffer Dalby <kradalby@kradalby.no>
2020-07-17 09:17:00 +0100

    make while loop to repeat to make it neater

--- Sources/MuninKit/Gallery.swift
+++ Sources/MuninKit/Gallery.swift
@@ -200,15 +200,15 @@ public struct Gallery {
 
     let bar = PercentProgressAnimation(stream: TSCBasic.stdoutStream, header: "Writing images")
 
-    while !ctx.queues.write.operations.isEmpty {
-      let executionCount = ctx.queues.write.operations.filter({ $0.isExecuting }).count
+    repeat {
+      let executionCount = 0  // ctx.queues.write.operations.filter({ $0.isExecuting }).count
       bar.update(
         step: totalOperations - ctx.queues.write.operationCount, total: totalOperations,
         text: "Left: \(ctx.queues.write.operationCount), parallel: \(executionCount)")
       usleep(
         300000  // Sleep
       )
-    }
+    } while !ctx.queues.write.operations.isEmpty
 
     ctx.queues.write.waitUntilAllOperationsAreFinished()
     bar.complete(success: ctx.queues.write.operations.isEmpty)
