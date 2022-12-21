a5baa0d3bad6b991263d721fe6e76d88086becc3
Kristoffer Dalby <kradalby@kradalby.no>
2022-12-21 17:07:16 +0100

    Testing
    Signed-off-by: Kristoffer Dalby <kradalby@kradalby.no>

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    version =
+      if (self ? shortRev)
+      then self.shortRev
+      else "dev";
+  in
+    {
+      overlay = final: prev: let
+        pkgs = nixpkgs.legacyPackages.${prev.system};
+      in rec {
+        munin = pkgs.stdenv.mkDerivation rec {
+          name = "munin";
+          src = ./.;
+
+          buildInputs = with pkgs;
+            [
+              clang
+              coreutils
+              imagemagick
+              libexif
+              libiptcdata
+            ]
+            ++ lib.optionals pkgs.stdenv.isLinux [
+              swift
+              swift-corelibs-libdispatch
+            ];
+
+          buildPhase = let
+            # On macOS, we have to use the Xcode bundled Swift
+            swiftBin =
+              if pkgs.stdenv.isLinux
+              then "${pkgs.swift}/bin/swift"
+              else "/usr/bin/swift";
+          in ''
+            # ${swiftBin} package resolve
+            ${swiftBin} build -v --configuration release --skip-update
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./.build/release/${name} $out/bin/${name}
+          '';
+        };
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        overlays = [self.overlay];
+        inherit system;
+      };
+    in rec {
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [
+            clang
+            coreutils
+            imagemagick
+            libexif
+            libiptcdata
+          ]
+          ++ lib.optionals pkgs.stdenv.isLinux [
+            swift
+            swift-corelibs-libdispatch
+          ];
+      };
+
+      apps = {
+        inherit (pkgs) munin;
+      };
+
+      defaultApp = pkgs.munin;
+
+      # `nix build`
+      packages = with pkgs; {
+        inherit munin;
+      };
+      defaultPackage = pkgs.munin;
+    });
+}
