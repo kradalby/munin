503b6ffc83ab39a29935d2337371e54462afddaa
Kristoffer Dalby <kradalby@kradalby.no>
2022-12-22 21:38:06 +0000

    Replace docker stuff mostly with Nix (#48)
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // https://theswiftdev.com/the-swift-package-manifest-file/
@@ -35,16 +35,16 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/kradalby/MagickWand.git", .branch("main")),
-    .package(url: "https://github.com/kradalby/SwiftExif.git", from: "0.0.6"),
+    .package(url: "https://github.com/kradalby/MagickWand.git", branch: "test"),
+    .package(url: "https://github.com/kradalby/SwiftExif.git", from: "0.0.7"),
+    // .package(path: "../SwiftExif"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
     .package(url: "https://github.com/Kitura/Configuration.git", from: "3.0.200"),
-    .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.1"),
-    // .package(url: "https://github.com/crspybits/swift-log-file.git", from: "0.1.0"),
+    .package(url: "https://github.com/onevcat/Rainbow.git", from: "4.0.1"),
     .package(
       url: "https://github.com/apple/swift-tools-support-core.git",
       .upToNextMajor(from: "0.2.4")),
-    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.0.2")),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
   ],
   targets: [
     .executableTarget(
@@ -55,14 +55,12 @@ let package = Package(
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
         "Configuration",
       ]
-      // linkerSettings: linkerSettings
     ),
     .target(
       name: "MuninKit",
       dependencies: [
-        .product(name: "SwiftToolsSupport", package: "swift-tools-support-core"),
+        .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
         .product(name: "Logging", package: "swift-log"),
-        // .product(name: "FileLogging", package: "swift-log-file"),
         "MagickWand",
         "SwiftExif",
         "Configuration",
