117739f9202099faaa8a35c9ec50571d9cabd725
Kristoffer Dalby <kristoffer@tailscale.com>
2024-02-06 19:47:17 +0100

    no support linux
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ var linkerSettings: [LinkerSetting]? {
 let package = Package(
   name: "Munin",
   platforms: [
-    .macOS(.v10_15)
-    .linux
+    .macOS(.v10_15),
+    // .linux
   ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
